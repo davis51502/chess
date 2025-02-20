a0eacf47e2e5a38ea3ef149efb22d91b0307a02b
Davis Wollesen <daviswollesen@gmail.com>
2025-02-20 10:37:59 -0700

    dependencies and webpage setup for testing

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,19 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        Server server = new Server();
+        int port = server.run(8080);
+
+        // confirm server is running
+        if (port != -1) {
+            System.out.println("Server is running on http://localhost:" + port);
+        } else {
+            System.err.println("Failed to start the server.");
+        }
     }
 }
