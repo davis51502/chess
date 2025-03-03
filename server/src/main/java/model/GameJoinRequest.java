8d22d2d6f386b87eb1c41852af914b8b542597c4
Davis Wollesen <daviswollesen@gmail.com>
2025-03-03 16:55:56 -0700

    structure

--- server/src/main/java/model/GameJoinRequest.java
+++ server/src/main/java/model/GameJoinRequest.java
@@ -1,13 +1,61 @@
-package model ;
+package model;
+
+/**
+ * Represents a request to join a game.
+ * Contains the player's chosen color and the game ID they wish to join.
+ */
 public class GameJoinRequest {
+
+    // The color chosen by the player (e.g., "WHITE" or "BLACK")
     private String playerColor;
+
+    // The ID of the game the player wants to join
     private int gameID;
+
+    /**
+     * Constructor to initialize a GameJoinRequest with the specified player color and game ID.
+     *
+     * @param playerColor The color chosen by the player (e.g., "WHITE" or "BLACK").
+     * @param gameID      The ID of the game the player wants to join.
+     */
     public GameJoinRequest(String playerColor, int gameID) {
         this.playerColor = playerColor;
         this.gameID = gameID;
     }
-    public String getPlayerColor() {return playerColor; }
-    public void setPlayerColor(String playerColor) {this.playerColor = playerColor;}
-    public int getGameID() {return gameID;}
-    public void setGameID(int gameID) {this.gameID = gameID;}
+
+    /**
+     * Gets the color chosen by the player.
+     *
+     * @return The player's chosen color (e.g., "WHITE" or "BLACK").
+     */
+    public String getPlayerColor() {
+        return playerColor;
+    }
+
+    /**
+     * Sets the color chosen by the player.
+     *
+     * @param playerColor The player's chosen color (e.g., "WHITE" or "BLACK").
+     */
+    public void setPlayerColor(String playerColor) {
+        this.playerColor = playerColor;
+    }
+
+    /**
+     * Gets the ID of the game the player wants to join.
+     *
+     * @return The game ID.
+     */
+    public int getGameID() {
+        return gameID;
+    }
+
+    /**
+     * Sets the ID of the game the player wants to join.
+     *
+     * @param gameID The game ID.
+     */
+    public void setGameID(int gameID) {
+        this.gameID = gameID;
+    }
 }
