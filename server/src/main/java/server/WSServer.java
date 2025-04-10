680165ffb87b9a801569f1e4fe63cce3416d52c4
Davis Wollesen <daviswollesen@gmail.com>
2025-04-10 13:45:51 -0600

    imports

--- server/src/main/java/server/WSServer.java
+++ server/src/main/java/server/WSServer.java
@@ -1,15 +1,29 @@
 package server;
 
 import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dataaccess.AuthDAO;
 import dataaccess.GameDAO;
+import model.AuthData;
 import model.ChessGameDeserializer;
-import org.eclipse.jetty.websocket.api.Session;
+import model.GameData;
+import org.eclipse.jetty.websocket.api.annotations.*;
+import org.eclipse.jetty.websocket.api.*;
+import websocket.commands.ConnectCommand;
+import websocket.commands.LeaveCommand;
+import websocket.commands.MakeMoveCommand;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
 
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 public class WSServer {
     private final AuthDAO authDAO = new AuthDAO();
@@ -19,7 +33,14 @@ public class WSServer {
             .registerTypeAdapter(ChessGame.class, new ChessGameDeserializer()).create();
     private static final Map<Integer, Set<Session>> gameSessions = new ConcurrentHashMap<>();
     private static final Map<Session, Integer> sessionToGameMap = new ConcurrentHashMap<>();
-    public WSServer() {
 
+    public WSServer() {
     }
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws Exception {
+        UserGameCommand command = gson.fromJson(message, UserGameCommand.class);
+        AuthData auth;
+        GameData game;
+        ChessGame.TeamColor playerColor;
 }
