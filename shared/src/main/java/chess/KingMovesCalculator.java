0e93239a1a979ffcef0027594d72baa3fa9a9e13
Davis Wollesen <daviswollesen@gmail.com>
2025-06-24 16:25:58 -0700

    king calculator pass

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,12 +1,43 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class KingMovesCalculator {
-    public KingMovesCalculator(ChessGame.TeamColor teamColor) {
+    private final ChessGame.TeamColor pieceColor;
+    public KingMovesCalculator(ChessGame.TeamColor pieceColor) {
+        this.pieceColor = pieceColor;
     }
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-    return null;
+        Collection<ChessMove> allKingMoves  = new ArrayList<>();
+        int startRow = myPosition.getRow();
+        int startCol = myPosition.getColumn();
+        int[][] staticSteps = {
+                {-1, -1},
+                {-1,  0},
+                {-1,  1},
+                { 0, -1},
+                { 0,  1},
+                { 1, -1},
+                { 1,  0},
+                { 1,  1}
+        };
+        for (int[] step : staticSteps) {
+            int rowChange = step[0];
+            int colChange = step[1];
+
+            int newRow = startRow + rowChange;
+            int newCol = startCol + colChange;
+
+            if (PieceMovesCalculator.isinBounds(newRow, newCol)) {
+                ChessPosition newPos = new ChessPosition(newRow, newCol);
+                ChessPiece targetPiece = board.getPiece(newPos);
+                if (targetPiece == null || targetPiece.getTeamColor() != this.pieceColor){
+                    allKingMoves.add(new ChessMove(myPosition, newPos, null));
+                }
+            }
+        }
+        return allKingMoves;
     }
 }
