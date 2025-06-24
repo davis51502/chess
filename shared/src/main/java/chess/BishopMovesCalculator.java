ed3878965123fd1fa0c847ef6ecffb2949c0fa51
Davis Wollesen <daviswollesen@gmail.com>
2025-06-24 16:04:54 -0700

    rook and bishop tests

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -1,13 +1,24 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class BishopMovesCalculator {
+    private final ChessGame.TeamColor pieceColor;
 
-    public BishopMovesCalculator(ChessGame.TeamColor teamColor) {
+    public BishopMovesCalculator(ChessGame.TeamColor pieceColor) {
+        this.pieceColor = pieceColor;
     }
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        Collection<ChessMove> allBishopMoves  = new ArrayList<>();
+        int startRow = myPosition.getRow();
+        int startCol = myPosition.getColumn();
+        allBishopMoves.addAll(PieceMovesCalculator.straightLiner(startRow, startCol, board, 1, 1, pieceColor));
+        allBishopMoves.addAll(PieceMovesCalculator.straightLiner(startRow, startCol, board, 1, -1, pieceColor));
+        allBishopMoves.addAll(PieceMovesCalculator.straightLiner(startRow, startCol, board, -1, 1, pieceColor));
+        allBishopMoves.addAll(PieceMovesCalculator.straightLiner(startRow, startCol, board, -1, -1, pieceColor));
+
+        return allBishopMoves;
     }
 }
--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -1,13 +1,22 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class RookMovesCalculator {
-
-    public RookMovesCalculator(ChessGame.TeamColor teamColor) {
+    private final ChessGame.TeamColor pieceColor;
+    public RookMovesCalculator(ChessGame.TeamColor pieceColor) {
+        this.pieceColor = pieceColor;
     }
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        Collection<ChessMove> allRookMoves  = new ArrayList<>();
+        int startRow = myPosition.getRow();
+        int startCol = myPosition.getColumn();
+        allRookMoves.addAll(PieceMovesCalculator.straightLiner(startRow, startCol, board, 1, 0, pieceColor));
+        allRookMoves.addAll(PieceMovesCalculator.straightLiner(startRow, startCol, board, 0, 1, pieceColor));
+        allRookMoves.addAll(PieceMovesCalculator.straightLiner(startRow, startCol, board, -1, 0, pieceColor));
+        allRookMoves.addAll(PieceMovesCalculator.straightLiner(startRow, startCol, board, 0, -1, pieceColor));
+        return allRookMoves;
     }
 }
