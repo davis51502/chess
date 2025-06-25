ed056dfd9b723f0b94bad041dc2a231491ddfacb
Davis Wollesen <daviswollesen@gmail.com>
2025-06-25 09:23:41 -0700

    pawn start

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -1,11 +1,37 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class PawnMovesCalculator {
-    public PawnMovesCalculator(ChessGame.TeamColor teamColor) {
+    private final ChessGame.TeamColor pieceColor;
+    public PawnMovesCalculator(ChessGame.TeamColor pieceColor) {
+        this.pieceColor = pieceColor;
     }
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-    return null;}
+        Collection<ChessMove> allPawnMoves = new ArrayList<>();
+        int startRow = myPosition.getRow();
+        int startCol = myPosition.getRow();
+
+        int forwardMove;
+        int startingRow;
+        int promotionRow;
+        if (pieceColor == ChessGame.TeamColor.WHITE) {
+            forwardMove = 1;
+            startingRow = 2;
+            promotionRow = 8;
+        } else {
+            forwardMove = -1;
+            startingRow = 7;
+            promotionRow = 1;
+        }
+        return allPawnMoves;
+    }
+    private void addPromotion(ChessPosition start, ChessPosition end, Collection<ChessMove> movesCollector) {
+        movesCollector.add(new ChessMove(start, end, ChessPiece.PieceType.QUEEN));
+        movesCollector.add(new ChessMove(start, end, ChessPiece.PieceType.ROOK));
+        movesCollector.add(new ChessMove(start, end, ChessPiece.PieceType.BISHOP));
+        movesCollector.add(new ChessMove(start, end, ChessPiece.PieceType.KNIGHT));
+    }
 }
