2aaed1bcd5dd332b7e3983f2925999711e17d58b
Davis Wollesen <daviswollesen@gmail.com>
2025-07-08 17:16:22 -0700

    Game service start

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,6 +1,29 @@
 package service;
-// create, join, list result
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.GameData;
+
+import java.util.Collection;
+
+//  list result. create, join
 public class GameService {
-    public GameService() {
+    private DataAccess dataAccess;
+    public GameService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
     }
+    public Collection<GameData> listGames(String authToken) throws DataAccessException {
+        if (dataAccess.getAuth(authToken) == null) {
+            throw new DataAccessException("error: unauthorized");
+        }
+        return dataAccess.listGames();
+    }
+    public int createGame(String authToken, String name) throws DataAccessException {
+        if (dataAccess.getAuth(authToken) == null) {
+            throw new DataAccessException("error: unauthorized");
+        }
+        return dataAccess.createGame(name);
+    }
+    public void joinGame(String)
+
 }
