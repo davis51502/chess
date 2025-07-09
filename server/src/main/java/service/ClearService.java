1b6c2afc404a60b8fc14ff376ae403ddbe8ca8fd
Davis Wollesen <daviswollesen@gmail.com>
2025-07-08 17:49:54 -0700

    service implemented

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,6 +1,15 @@
 package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+
 // clear result
 public class ClearService {
-    public ClearService() {
+    private DataAccess dataAccess;
+    public ClearService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+    public void clearApp() throws DataAccessException {
+        dataAccess.clear();
     }
 }
--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -2,7 +2,7 @@ package service;
 
 import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
-import model.GameData;
+import model.*;
 
 import java.util.Collection;
 
@@ -24,6 +24,32 @@ public class GameService {
         }
         return dataAccess.createGame(name);
     }
-    public void joinGame(String)
+    public void joinGame(String authToken, String gamerColor, int gameID) throws DataAccessException {
+        // check if user is authorized
+        if (dataAccess.getAuth(authToken) == null) {
+            throw new DataAccessException("error: unauthorized");
+        }
+        // check if the game exists
+        if (dataAccess.getGame(gameID) == null) {
+            throw new DataAccessException("can't do that: invalid request");
+        }
+        // check which side the player wants to join: black or white
+        if (gamerColor.equalsIgnoreCase("WHITE")) {
+            if (dataAccess.getGame(gameID).whiteUsername() != null) {
+                throw new DataAccessException("error: already in use ");
+            }
+            GameData gameUpdate = new GameData(gameID, dataAccess.getAuth(authToken).username(),
+                    dataAccess.getGame(gameID).blackUsername(), dataAccess.getGame(gameID).gameName());
+            dataAccess.updateGame(gameUpdate);
+        } else if (gamerColor.equalsIgnoreCase("BLACK")) {
+            if (dataAccess.getGame(gameID).whiteUsername() != null) {
+                throw new DataAccessException("error: already in use ");
+            }
+            GameData gameUpdate = new GameData(gameID, dataAccess.getAuth(authToken).username(),
+                    dataAccess.getGame(gameID).whiteUsername(), dataAccess.getGame(gameID).gameName());
+            dataAccess.updateGame(gameUpdate);
 
+    }
+
+}
 }
