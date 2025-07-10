e2ab1dd9e5d82c2fd7a3f359a2b815a75d332b4b
Davis Wollesen <daviswollesen@gmail.com>
2025-07-10 15:27:58 -0700

    handlers

--- server/src/main/java/handlers/ClearHandler.java
+++ server/src/main/java/handlers/ClearHandler.java
@@ -1,4 +1,31 @@
 package handlers;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import service.ClearService;
+import spark.Request;
+import spark.Response;
+
+import java.util.Map;
+
 public class ClearHandler {
+    private ClearService clearService;
+    private Gson gson;
+    public ClearHandler(ClearService clearService) {
+        this.clearService = clearService;
+        this.gson = new Gson();
+    }
+    public Object clearApp(Request req, Response res){
+        try{
+            clearService.clearApp();
+            res.status(200);
+            return "{}";
+        } catch (DataAccessException e) {
+            res.status(500);
+            return gson.toJson(Map.of("message", "Error: " + e.getMessage()));
+        } catch (Exception e) {
+            res.status(500);
+            return gson.toJson(Map.of("message", "Error: " + e.getMessage()));
+        }
+    }
 }
--- server/src/main/java/handlers/GameHandler.java
+++ server/src/main/java/handlers/GameHandler.java
@@ -69,6 +69,44 @@ public class GameHandler {
                 return gson.toJson(Map.of("message", "Error: " + e.getMessage()));
             }
             }
+        public Object joinGame(Request req, Response res) {
+        try{
+        String authToken = req.headers("Auth");
+            if (authToken == null || authToken.isEmpty()) {
+                res.status(401);
+                return gson.toJson(Map.of("message", "Error: unauthorized"));
+            }
+            Map reqData = gson.fromJson(req.body(), Map.class);
+            String juego = (String) reqData.get("gameName") ;
+            Double gameDouble = (Double) reqData.get("gameID");
+            Integer gameID = gameDouble.intValue();
+            if (gameID == null) {
+                res.status(400);
+                return gson.toJson(Map.of("message", "Error: bad request"));
+            }
+            // playerColor is allowed to be null for observers
+            gameService.joinGame(authToken, juego, gameID);
+            res.status(200);
+            return "{}";
 
+        } catch (DataAccessException e) {
+            if (e.getMessage().contains("unauthorized") || e.getMessage().contains("invalid")) {
+                res.status(401);
+                return gson.toJson(Map.of("message", "Error: unauthorized"));
+            } else if (e.getMessage().contains("already taken") || e.getMessage().contains("taken")){
+                res.status(403);
+                return gson.toJson(Map.of("message", "Error: already taken"));
+            } else if (e.getMessage().contains("bad req")){
+                res.status(400);
+                return gson.toJson(Map.of("message", "Error: bad req"));
+            }else {
+                res.status(500);
+                return gson.toJson(Map.of("message", "Error: " + e.getMessage()));
+            }
+        } catch (Exception e) {
+            res.status(500);
+            return gson.toJson(Map.of("message", "Error: " + e.getMessage()));
         }
+    }
+}
 
