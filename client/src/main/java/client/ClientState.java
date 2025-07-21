8cc7444402af78a4b2e25f0107f030c3d998143f
Davis Wollesen <daviswollesen@gmail.com>
2025-07-21 15:40:46 -0700

    server facade setup

--- client/src/main/java/client/ClientState.java
+++ client/src/main/java/client/ClientState.java
@@ -3,32 +3,75 @@ package client;
 import chess.ChessGame;
 import model.GameData;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // holds session data like auth and game states
 public class ClientState {
     private String authToken;
     private String username;
     private GameData currentGame;
+    private List<GameData> gamesList = new ArrayList<>();
     private ChessGame.TeamColor playerColor;
     private boolean inGame = false;
+
     public boolean isLoggedIn() {
         return authToken != null;
     }
+
     public boolean isInGame() {
         return inGame;
     }
+
     public String getPrompted() {
         if (!isLoggedIn()) return "[LOGGED_OUT] >>> ";
         if (!isInGame()) return "[LOGGED_IN] >>> ";
         return "[IN_GAME] >>> ";
     }
-    public String getAuthToken() {return authToken;}
-        public void setAuthToken(String authToken) {this.authToken = authToken;}
-    public String getUsername() {return username;}
-        public void setUsername(String username) {this.username = username;}
-    public void setInGame(boolean inGame) {this.inGame = inGame;}
-    public GameData getCurrGame() {return currentGame;}
-        public void setCurrentGame(GameData currentGame) {this.currentGame = currentGame;}
-    public ChessGame.TeamColor getWhichPlayerColor() {return playerColor;}
-    public void
 
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setInGame(boolean inGame) {
+        this.inGame = inGame;
+    }
+
+    public GameData getCurrGame() {
+        return currentGame;
+    }
+
+    public void setCurrentGame(GameData currentGame) {
+        this.currentGame = currentGame;
+    }
+
+    public ChessGame.TeamColor getWhichPlayerColor() {
+        return playerColor;
+    }
+
+    public void setPlayerColor(ChessGame.TeamColor playerColor) {
+        this.playerColor = playerColor;
+    }
+
+    public List<GameData> getGamesList() {
+        return gamesList;
+    }
+
+    public void setGamesList(List<GameData> gamesList) {
+        this.gamesList = gamesList;
+    }
 }
+
+
--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,40 @@
 package client;
+
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.AuthData;
+import model.GameData;
+
+import java.util.Collection;
+
 // handles http connections
 public class ServerFacade {
+    private final String serverURL;
+    private final Gson gson = new Gson();
+    public ServerFacade(String serverURL) {
+        this.serverURL = serverURL;
+    }
+    public AuthData register() {
+
+    }
+    public AuthData login() {
+
+    }
+    public void logout() {
+
+    }
+    public GameData createGame() {
+
+    }
+    public Collection<GameData> listGames() {
+
+    }
+    public void joinGame() {
+
+    }
+    private <initialType> initialType makeReq(String method, String path,
+                                              Object req, Class<initialType> responseClass, String authToken) throws Exception {
+
+    }
+    private record JoinGameReq(ChessGame.TeamColor playerColor, int gameID) {}
 }
