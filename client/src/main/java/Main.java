f879674fd4012828f22feb2557d9bfcc62f20320
Davis Wollesen <daviswollesen@gmail.com>
2025-07-21 14:54:06 -0700

    structure initilization: phase 5

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import chess.*;
-
+// this is the conductor
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        String serverURL = args.length > 0 ? args[0] : "https://localhost:3000";
+        new
     }
 }
