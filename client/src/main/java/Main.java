082d7b5a07621c6a2d29d7f21bf7b770b5f7381a
Davis Wollesen <daviswollesen@gmail.com>
2025-07-23 12:53:42 -0700

    post login create, list, help and setup

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,9 +1,12 @@
 import chess.*;
+import client.ChessClient;
+
 // this is the entry point
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
         String serverURL = args.length > 0 ? args[0] : "https://localhost:3000";
+        new ChessClient(serverURL, ).run();
     }
 }
--- client/src/main/java/client/handlers/PostLogin.java
+++ client/src/main/java/client/handlers/PostLogin.java
@@ -1,4 +1,68 @@
 package client.handlers;
+
+import client.ClientState;
+import client.ServerFacade;
+import client.ui.BoardGenerator;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 // create list join and logout
 public class PostLogin {
+
+    private final ServerFacade serverFacade;
+    private final ClientState state ;
+    private final BoardGenerator boardGenerator;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public PostLogin(ServerFacade serverFacade, ClientState state, BoardGenerator boardGenerator) {
+        this.serverFacade = serverFacade;
+        this.state = state;
+        this.boardGenerator = boardGenerator;
+    }
+    public void handle(String... args) throws Exception {
+        String command = args[0].toLowerCase();
+        String[] params = Arrays.copyOfRange(args, 1, args.length);
+        switch (command) {
+            case "create" -> create();
+            case "list" -> list();
+            case "play" -> join(false);
+            case "observe" -> join(true);
+            case "logout" -> logout();
+            case "help" -> help();
+            default -> System.out.println("invalid command, type 'help' for command options");
+        }
+    }
+    public void create() throws Exception {
+        System.out.print("game name: ");
+        String gameName = scanner.nextLine();
+        serverFacade.createGame(state.getAuthToken(), gameName);
+        System.out.println("game `" + gameName + "` created.");
+    }
+    public void list() throws Exception {
+        var games = serverFacade.listGames(state.getAuthToken());
+        state.setGamesList(new ArrayList<>(games));
+        if (state.getGamesList().isEmpty()) {
+            System.out.println("no games available");
+            return;
+        }
+        for (int i =0; i< state.getGamesList().size(); i++ ) {
+            GameData data = state.getGamesList().get(i);
+            System.out.printf("%d. %s (white: %s, black: %s) %n ",
+                    i +1 , data.gameName(), data.whiteUsername(), data.blackUsername());
+        }
+    }
+    private void help() throws Exception {
+        System.out.println("""
+                Available commands: 
+                create = create anew game 
+                list - list all games 
+                play = join game as player  
+                observe = join game as observer 
+                logout = sign out of your account 
+                help - show this message again 
+                """);
+    }
 }
